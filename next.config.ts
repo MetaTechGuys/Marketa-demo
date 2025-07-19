0c34c4d7bd63e7a37c8082bc9fe11ef887e616f0
Ali Ghorbani <alidoubleplus@gmail.com>
2025-07-19 19:50:49 +0330

    remove cloudflare pages

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
-import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev'
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   pageExtensions: ['ts', 'tsx'],
 }
-
-if (process.env.NODE_ENV === 'development') {
-  await setupDevPlatform()
-}
-
 // Merge MDX config with Next.js config
 export default nextConfig
