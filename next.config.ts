14de04aa8815ecd64127daacd18785252a47590c
Ali Ghorbani <alidoubleplus@gmail.com>
2025-07-19 18:47:51 +0330

    npm audit fix

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   pageExtensions: ['ts', 'tsx'],
+  externals: [
+    {
+      sharp: 'commonjs sharp',
+    },
+  ],
 }
 
 // Merge MDX config with Next.js config
